f2c91c3489580ecbb68ca6d910464fe2ddbafea7
Rui Yang <ruy204github@gmail.com>
2019-08-26 16:12:35 -0400

    SV specific

--- Visualization_R.R
+++ Visualization_R.R
@@ -14,13 +14,18 @@ library(BioCircos)
 # color extraction function
 f <- function(pal) brewer.pal(brewer.pal.info[pal, "maxcolors"], pal)
 
-#################
-#   Functions   #
-#################
+bedpe %>% ggplot(aes(start1,Description,color=type))+geom_point(size=3)+
+  theme_bw()+facet_wrap(~chrom1)+labs(title="General Overview")
+
+#######################
+#   Basic Functions   #
+#######################
 
 #1. check specific region on chromosome
 
-chromosome_check<-function(chr,start_position,chunck_length,annotate="population",types=unique(bedpe$type)){
+chromosome_check<-function(chr,start_position,chunck_length,annotate="population",
+                           types=c("inversion","deletion","reciprocal","inverted-reciprocal",
+                                   "inverted-duplication","duplication","translocation","inverted-translocation")){
   
   bedpe<-read.delim("C:/PhD/Rotations/Rotation_1/data/SV2/bedpe/combine/jointdf.bedpe",header = F)
   colnames(bedpe)<-c("chrom1","start1","end1","chrom2","start2","end2",
@@ -49,7 +54,12 @@ chromosome_check<-function(chr,start_position,chunck_length,annotate="population
   #3. create data track
   
   lim<-c(start_position,end_position)
-  subdata = bedpe %>% dplyr::filter(chrom1==chr, start1 %in% seq(start_position,end_position,1))
+  # subdata = bedpe %>% dplyr::filter(chrom1==chr, start1 %in% seq(start_position,end_position,1))
+  subdata = bedpe %>% dplyr::filter(chrom1==chr, start1 >= start_position & start1 <= end_position)
+  subdata$strand<-"*"
+  subdata$strand[which(subdata$start2>subdata$start1)]<-"+"
+  subdata$strand[which(subdata$start2<subdata$start1)]<-"-"
+  
   dat<-as.numeric(subdata$type)
   coords <- sort(c(subdata$start1,lim[2]))
   
@@ -58,7 +68,7 @@ chromosome_check<-function(chr,start_position,chunck_length,annotate="population
   
   aTrack<-AnnotationTrack(start=subdata$start1,width = abs(subdata$end1-subdata$start1),chromosome = chr,
                           group=subdata$samples,genome="hg19",name="SV",
-                          stacking = "squish")
+                          stacking = "squish",strand = subdata$strand)
   
   if(annotate=="population"){
     identifier(aTrack)=as.character(subdata$Description)
@@ -69,10 +79,12 @@ chromosome_check<-function(chr,start_position,chunck_length,annotate="population
   
   plotTracks(list(itrack,gtrack,biomTrack,aTrack), col.line = NULL, col = NULL,
              from=start_position,to=end_position,extend.left = 0.1,extend.right = 0.1,
-             background.panel="#FFFEDB", background.title="darkblue",groupAnnotation="group")
+             background.panel="#FFFEDB", background.title="darkblue",groupAnnotation="group",
+             showOverplotting=TRUE,collapse=TRUE)
 }
 
-chromosome_check("chr22",21000000,100000)
+# chromosome_check("chr22",21000000,100000)
+chromosome_check("chr1",   149000000,500000)
 
 #2. check intra-chromosome events for specific chromosome/region
 
@@ -98,7 +110,7 @@ intra_check<-function(chromosome,start,end,plot_type="loops"){
   df1$end<-as.numeric(as.character(df1$end))
   
   chra = chromosome
-  sub<-bedpe %>% dplyr::filter(chrom1==chra & chrom2==chra) %>% dplyr::arrange(start1)
+  sub<-bedpe %>% dplyr::filter(chrom1==chra & chrom2==chra, type %in% c("duplication","inverted-duplication")) %>% dplyr::arrange(start1)
   # sub<-bedpe %>% dplyr::filter(start1 %in% c(142685331,142690334))
   pbpe = plotBedpe(sub,chrom = chra,chromstart = max(1,start),
                    chromend=min(dplyr::filter(df1,Chrom1==chra)$end,end),
@@ -106,13 +118,16 @@ intra_check<-function(chromosome,start,end,plot_type="loops"){
                    colorby=as.numeric(as.factor(sub$type)),colorbycol=hcl.colors,
                    border="black",lwd = 5)
   labelgenome(chra, 1, dplyr::filter(df1,Chrom1==chra)$end,side=1,scipen=20,n=30,scale="Mb",line=.18,chromline=.5,scaleline=0.5)
-  legend("topright",inset =0.01,legend=levels(as.factor(sub$type)),col=hcl.colors(length(levels(as.factor(sub$type)))),pch=19,bty='n',text.font=2)
+  legend("topright",inset =0.01,
+         legend=levels(as.factor(as.character(sub$type))),
+         col=hcl.colors(length(levels(as.factor(as.character(sub$type))))),
+         pch=19,bty='n',text.font=2)
   # legend("topright",inset =0.01,legend=levels(as.factor(sub$samples)),col=f("Set1")[1:length(levels(as.factor(sub$samples)))],pch=19,bty='n',text.font=2)
   axis(side=2,las=2,tcl=.2)
   
 }
 
-intra_check("chr22",15000000,30000000)
+# intra_check("chr22",15000000,30000000)
 
 #3. inter-chromosome activities
 
@@ -150,7 +165,7 @@ inter_check<-function(events){
             TEXTModuleDragEvent = TRUE)
 }
 
-inter_check("inverted-reciprocal")
+# inter_check("inverted-reciprocal")
 
 #4. number of samples for inter-chromosome activities
 
@@ -190,7 +205,190 @@ inter_count<-function(events,resolution=1){
   
 }
 
-inter_count("inverted-reciprocal")
+# inter_count("inverted-reciprocal")
+
+###############################
+#   Specific Visualizations   #
+###############################
+
+#1. For deletion and inversion only 
+
+intra_karo<-function(events){
+  
+  ethrank = c('Finnish','SOUTHERN HAN CHINESE','PUERTO RICAN','UTAH/MORMON','YORUBA/Nigeria','HAN CHINESE/China',
+              'JAPANESE/Japan','USA/MEXICAN','USA/AFRICAN','ITALY/TOSCANI','Caucasian')
+  
+  #bedpe file
+  bedpe<-read.delim("C:/PhD/Rotations/Rotation_1/data/SV2/bedpe/combine/jointdf.bedpe",header = F)
+  colnames(bedpe)<-c("chrom1","start1","end1","chrom2","start2","end2",
+                     "type","score", "samples","Description")
+  bedpe[,c(2,3,5,6,8)]<-sapply(bedpe[,c(2,3,5,6,8)],function(x){as.numeric(as.character(x))})
+  bedpe$chrom1<-factor(bedpe$chrom1,levels=paste("chr",c(1:22,"X","Y"),sep=""))
+  bedpe$chrom2<-factor(bedpe$chrom2,levels=paste("chr",c(1:22,"X","Y"),sep=""))
+  bedpe$Description<-factor(bedpe$Description,levels=ethrank)
+  
+  #chromosome preparation
+  df1=as.data.frame(cbind(paste("chr",c(1:22,"X","Y"),sep=""),rep(1,24),
+                          c(249250621,243199373,198022430,191154276,180915260,171115067,159138663,146364022,141213431,135534747,
+                            135006516,133851895,115169878,107349540,102531392,90354753,81195210,78077248,59128983,63025520,
+                            48129895,51304566,155270560,59373566)))
+  colnames(df1)<-c("Chrom1","start","end")
+  df1$Chrom1<-factor(df1$Chrom1,levels=paste("chr",c(1:22,"X","Y"),sep=""))
+  df1$end<-as.numeric(as.character(df1$end))
+  
+  sub<-bedpe %>% dplyr::filter(type %in% events) %>% rowwise() %>% 
+    mutate(start=min(start1,start2),end=max(end1,end2)) %>% ungroup()
+  ggplot(data=df1,aes(Chrom1,end))+geom_bar(stat="identity",color="black",fill="white",width=0.5)+theme_bw()+coord_flip()+
+    ggplot2::geom_segment(data=sub,aes(x=chrom1,xend=chrom1,y=start,yend=end,color=Description),size=6)+
+    scale_x_discrete(limits = rev(levels(df1$Chrom1)))+scale_color_brewer(palette = "Paired")+
+    facet_wrap(~type)
+}
+
+intra_karo(c("deletion","inversion"))
+
+#2. for reciprocal, translocation (inverted) only 
+
+inter_check_multi<-function(events){
+  
+  #read-in bedpe data
+  bedpe<-read.delim("C:/PhD/Rotations/Rotation_1/data/SV2/bedpe/combine/jointdf.bedpe",header = F)
+  colnames(bedpe)<-c("chrom1","start1","end1","chrom2","start2","end2",
+                     "type","score", "samples","Description")
+  bedpe[,c(2,3,5,6,8)]<-sapply(bedpe[,c(2,3,5,6,8)],function(x){as.numeric(as.character(x))})
+  bedpe$chrom1<-factor(bedpe$chrom1,levels=paste("chr",c(1:22,"X","Y"),sep=""))
+  bedpe$chrom2<-factor(bedpe$chrom2,levels=paste("chr",c(1:22,"X","Y"),sep=""))
+  bedpe$length1<-bedpe$end1-bedpe$start1
+  bedpe$length2<-bedpe$end2-bedpe$start2
+  
+  tracklist = BioCircosBackgroundTrack("myBackgroundTrack", minRadius = 0, maxRadius = 1,
+                                       borderSize = 0, fillColors = "#EEFFEE")
+  
+  for(i in seq_along(events)){
+    
+    eventk = events[i]
+    sub<-bedpe %>% dplyr::filter(type %in% eventk)
+    links_chromosomes_1 = gsub("chr","",sub$chrom1)
+    links_chromosomes_2 = gsub("chr","",sub$chrom2)
+    links_pos_1 = sub$start1
+    links_pos_2 = sub$start2
+    links_labels = NA
+    
+    tracklist = tracklist + BioCircosLinkTrack('myLinkTrack', links_chromosomes_1, links_pos_1,
+                                               links_pos_1 , links_chromosomes_2, 
+                                               links_pos_2, links_pos_2 ,color=f("Set1")[i],
+                                               maxRadius = 1, labels = links_labels)
+    
+  }
+  
+  BioCircos(tracklist, genomeFillColor = "Paired",
+            chrPad = 0.02, displayGenomeBorder = F, yChr =  T,
+            genomeTicksDisplay = FALSE,  
+            genomeLabelTextSize = "10pt", genomeLabelDy = 0,zoom = T,
+            TEXTModuleDragEvent = TRUE)
+}
+
+inter_check_multi(c("reciprocal","inverted-reciprocal"))
+
+#3. for duplication (inverted), inversion, deletion
+
+intra_check_arch<-function(event="inverted-duplication",chromosome,start=0,end=250000000,plot_type="loops",colorby="Description"){
+  
+  #read-in bedpe data
+  bedpe<-read.delim("C:/PhD/Rotations/Rotation_1/data/SV2/bedpe/combine/jointdf.bedpe",header = F)
+  colnames(bedpe)<-c("chrom1","start1","end1","chrom2","start2","end2",
+                     "type","score", "samples","Description")
+  bedpe[,c(2,3,5,6,8)]<-sapply(bedpe[,c(2,3,5,6,8)],function(x){as.numeric(as.character(x))})
+  bedpe$chrom1<-factor(bedpe$chrom1,levels=paste("chr",c(1:22,"X","Y"),sep=""))
+  bedpe$chrom2<-factor(bedpe$chrom2,levels=paste("chr",c(1:22,"X","Y"),sep=""))
+  bedpe$length1<-bedpe$end1-bedpe$start1
+  bedpe$length2<-bedpe$end2-bedpe$start2
+  
+  #chromosome preparation
+  df1=as.data.frame(cbind(paste("chr",c(1:22,"X","Y"),sep=""),rep(1,24),
+                          c(249250621,243199373,198022430,191154276,180915260,171115067,159138663,146364022,141213431,135534747,
+                            135006516,133851895,115169878,107349540,102531392,90354753,81195210,78077248,59128983,63025520,
+                            48129895,51304566,155270560,59373566)))
+  colnames(df1)<-c("Chrom1","start","end")
+  df1$Chrom1<-factor(df1$Chrom1,levels=paste("chr",c(1:22,"X","Y"),sep=""))
+  df1$end<-as.numeric(as.character(df1$end))
+  
+  if(colorby=="Description"){
+    chra = chromosome
+    sub<-bedpe %>% dplyr::filter(chrom1==chra & chrom2==chra, type %in% event) %>% dplyr::arrange(start1)
+    # sub<-bedpe %>% dplyr::filter(start1 %in% c(142685331,142690334))
+    pbpe = plotBedpe(sub,chrom = chra,chromstart = max(1,start),
+                     chromend=min(dplyr::filter(df1,Chrom1==chra)$end,end),
+                     heights = sub$score,bty='n',plottype=plot_type,offset=1,flip=F,
+                     colorby=as.numeric(as.factor(sub$Description)),colorbycol=hcl.colors,
+                     border="black",lwd = 5)
+    labelgenome(chra, 1, dplyr::filter(df1,Chrom1==chra)$end,side=1,scipen=20,n=30,scale="Mb",line=.18,chromline=.5,scaleline=0.5)
+    legend("topright",inset =0.01,
+           legend=levels(as.factor(as.character(sub$Description))),
+           col=hcl.colors(length(levels(as.factor(as.character(sub$Description))))),
+           pch=19,bty='n',text.font=2)
+    # legend("topright",inset =0.01,legend=levels(as.factor(sub$samples)),col=f("Set1")[1:length(levels(as.factor(sub$samples)))],pch=19,bty='n',text.font=2)
+    axis(side=2,las=2,tcl=.2)
+  }else{
+    chra = chromosome
+    sub<-bedpe %>% dplyr::filter(chrom1==chra & chrom2==chra, type %in% event) %>% dplyr::arrange(start1)
+    # sub<-bedpe %>% dplyr::filter(start1 %in% c(142685331,142690334))
+    pbpe = plotBedpe(sub,chrom = chra,chromstart = max(1,start),
+                     chromend=min(dplyr::filter(df1,Chrom1==chra)$end,end),
+                     heights = sub$score,bty='n',plottype=plot_type,offset=1,flip=F,
+                     colorby=as.numeric(as.factor(sub$type)),colorbycol=hcl.colors,
+                     border="black",lwd = 5)
+    labelgenome(chra, 1, dplyr::filter(df1,Chrom1==chra)$end,side=1,scipen=20,n=30,scale="Mb",line=.18,chromline=.5,scaleline=0.5)
+    legend("topright",inset =0.01,
+           legend=levels(as.factor(as.character(sub$type))),
+           col=hcl.colors(length(levels(as.factor(as.character(sub$type))))),
+           pch=19,bty='n',text.font=2)
+    # legend("topright",inset =0.01,legend=levels(as.factor(sub$samples)),col=f("Set1")[1:length(levels(as.factor(sub$samples)))],pch=19,bty='n',text.font=2)
+    axis(side=2,las=2,tcl=.2)
+  }
+}
+
+intra_check_dup(unique(bedpe$type),"chr22",start=18000000,end=24000000,colorby="type")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
